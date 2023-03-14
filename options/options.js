8bda0315500916bf8e3742de231f6c71c943a11a
Crowdbotics <team@crowdbotics.com>
2023-03-14 01:00:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "round-cloud-39361",
+  url: "https://round-cloud-39361.botics.co",
+  api: "https://round-cloud-39361.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
